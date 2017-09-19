2ec8010464b2ab957f09a43cde7140a996119fdd
Unknown <njxqlus@gmail.com>
2017-09-19 14:45:19 +0300

    Update header

--- resources/views/cp/parts/header.blade.php
+++ resources/views/cp/parts/header.blade.php
@@ -1,7 +1,5 @@
-<!-- Main Header -->
 <header class="main-header">
 
-    <!-- Logo -->
     <a href="#" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b>A</b>LT</span>
@@ -9,23 +7,10 @@
         <span class="logo-lg"><b>Admin</b>LTE</span>
     </a>
 
-    <!-- Header Navbar -->
     <nav class="navbar navbar-static-top" role="navigation">
-        <!-- Sidebar toggle button-->
         <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
             <span class="sr-only">@lang('cp.toggle_navigation')</span>
         </a>
-        <!-- Navbar Right Menu -->
-        <div class="navbar-custom-menu">
-            <ul class="nav navbar-nav">
-                <li>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="padding: 10px;">
-                        {{ csrf_field() }}
-                        <button type="submit" class="btn btn-default btn-sm btn-flat"><i
-                                    class="fa fa-sign-out"></i> @lang('cp.sign_out')</button>
-                    </form>
-                </li>
-            </ul>
-        </div>
     </nav>
+    
 </header>
